308cbcfde7e4f22cf77168c6b831a72302a82df6
Christopher Coté <chris@entropealabs.com>
2018-02-09 09:44:23 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mdns.Mixfile do
 
   def project do
     [app: :mdns,
-     version: "0.1.6",
+     version: "0.1.7",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
