651b692da8719adb71d2da4690499e842bdd4b29
Christopher Coté <ccote@blockfi.com>
2020-08-11 09:12:37 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mdns.Mixfile do
   def project do
     [
       app: :mdns,
-      version: "1.0.5",
+      version: "1.0.6",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
